e03877732e417fd2481a29d1d83c29375c516c11
alireza <164399453+alirezag103@users.noreply.github.com>
2024-07-12 19:03:27 +0330

    More redirection and some style stuff

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+import dictionary
 
 urlpatterns = [
-    path('<str:username>/dictionaries/', views.get_dictionaries_list),
+    path('<str:username>/dictionaries/', views.get_dictionaries_list, name="user_dictionaries"),
     path('<str:username>/newdictionary/', views.create_dictionary),
-    path('<str:username>/<str:dictionary>/', views.get_dictionary_content),
+    path('<str:username>/<str:dictionary>/', views.get_dictionary_content, name="dictionary_content"),
 ]
