30a6a32da22ac440e9780e8bc5d2ecbdc6f6df06
alireza <164399453+alirezag103@users.noreply.github.com>
2024-07-25 23:11:03 +0330

    Switch to mysql as db

--- customdict/settings.py
+++ customdict/settings.py
@@ -84,9 +84,15 @@ WSGI_APPLICATION = 'customdict.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': str( BASE_DIR / 'my.cnf'),
+        }
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
